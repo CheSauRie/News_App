847940da5578381acb462a36ea14434fb2aa96be
Gia Minh Hoang <giaminh5567@gmail.com>
2023-07-26 07:30:17 +0700

    Layout for Weather Activity

--- app/src/main/java/com/example/myapplication/WeatherActivity.java
+++ app/src/main/java/com/example/myapplication/WeatherActivity.java
@@ -9,6 +9,6 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
+        setContentView(R.layout.activity_weather1);
     }
 }
